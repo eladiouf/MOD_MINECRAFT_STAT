a5fa2ea27969b76de97e2e7122690b814d8a566f
El Hadji <onivostudio@gmail.com>
2026-07-15 17:50:26 +0000

    feat: sync automatic combat perks

--- src/test/java/tong/statmod/network/StatProgressNoticeMessageTest.java
+++ src/test/java/tong/statmod/network/StatProgressNoticeMessageTest.java
@@ -40,6 +40,6 @@ class StatProgressNoticeMessageTest {
 
     @Test
     void bookStudyInputUsesTheCurrentWireProtocol() {
-        assertEquals("5", StatModRuntime.NETWORK_PROTOCOL);
+        assertEquals("6", StatModRuntime.NETWORK_PROTOCOL);
     }
 }
